d70550726f9724ed3c7f5476261bf67e4e810b30
Dane O'Connor <dane.oconnor@gmail.com>
2014-08-28 16:48:16 -0400

    Strip trailing whitespace

--- lib/suitor.rb
+++ lib/suitor.rb
@@ -1,8 +1,8 @@
-require 'redditkit' 
-require 'twilio-ruby' 
+require 'redditkit'
+require 'twilio-ruby'
 
 @client =RedditKit::Client.new "ruby_relay_bot", "PahkThaCah"
-body = @client.links("romance").first.title 
+body = @client.links("romance").first.title
 body += " "+ @client.links("romance").first.url
 
 @client = Twilio::REST::Client.new 'ACbb8840b48cc93d4b05e987d82f7a281a', '0f065f163e588424cf645b07e3dbc0cb'
