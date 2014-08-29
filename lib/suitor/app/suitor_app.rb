c7a60ed7033839909b6a6bf3a44cd110c61d53c8
Dane O'Connor <dane.oconnor@gmail.com>
2014-08-29 17:41:48 -0400

    Add sintra contrib to project to leverage json helpers

--- lib/suitor/app/suitor_app.rb
+++ lib/suitor/app/suitor_app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require 'sinatra/json'
 
 module Suitor
 module App
 
   class SuitorApp < Sinatra::Base
+    helpers Sinatra::JSON
     set :views, File.join(settings.root, "templates")
     set :haml, format: :html5
 
