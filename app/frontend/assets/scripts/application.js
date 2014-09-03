f3dd56160c6c8e1b1ed9298aa10e8a76774eb7b5
Dane O'Connor <dane.oconnor@gmail.com>
2014-09-02 21:42:19 -0400

    Create a simple backbone view to test new ajax endpoint

--- app/frontend/assets/scripts/application.js
+++ app/frontend/assets/scripts/application.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
   $('form input[name="subreddit"]').focus();
 });
+
+var CharmView = Backbone.View.extend({
+
+  el: $("#charm-form"),
+
+  events: {
+    "submit": "submit"
+  },
+
+  initialize: function() {
+  },
+
+  submit: function(e) {
+    $.ajax({
+      url: "/charm",
+      type: "POST",
+      accepts: "application/json",
+      data: this.$el.serialize(),
+      success: function(data) {
+        console.log(data);
+      }
+    });
+
+    return false; // don't actually submit of the form.
+  },
+});
+
+view = new CharmView();
