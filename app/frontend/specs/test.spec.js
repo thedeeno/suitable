756f715f5721ece0f627d173be28653b68356a5c
Dane O'Connor <dane.oconnor@gmail.com>
2014-09-03 13:00:51 -0400

    Configure passing test for karma

--- app/frontend/specs/test.spec.js
+++ app/frontend/specs/test.spec.js
@@ -1,5 +1,5 @@
 describe("A test suite", function() {
    beforeEach(function() { });
    afterEach(function() { });
-   it('should fail', function() { expect(true).to.be.false; });
+   it('should pass', function() { expect(true).to.be.true; });
 });
