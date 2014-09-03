2b727e7e3f59e362de682a0147c0236d84de52e8
Dane O'Connor <dane.oconnor@gmail.com>
2014-09-03 11:42:44 -0400

    Add bounce animation to speach bubble

--- app/frontend/dist/js/application.js
+++ app/frontend/dist/js/application.js
@@ -7,14 +7,23 @@ var CharmView = Backbone.View.extend({
   el: $("body"),
 
   events: {
-    "submit form": "submit"
+    "submit form": "submit",
+    "mouseup input[type='submit']": "blur"
   },
 
   initialize: function() {
     this.$form = $('form');
   },
 
+  blur: function(e) {
+    console.log(e);
+    button = $(e.target);
+    button.blur();
+  },
+
   submit: function(e) {
+    e.preventDefault();
+
     $.ajax({
       url: "/charm",
       type: "POST",
@@ -24,8 +33,6 @@ var CharmView = Backbone.View.extend({
         $(".last-charm").html(JST["assets/templates/last_charm.html"](data));
       }
     });
-
-    return false; // don't actually submit of the form.
   },
 });
 
--- app/frontend/dist/js/templates.js
+++ app/frontend/dist/js/templates.js
@@ -4,7 +4,7 @@ this["JST"]["assets/templates/last_charm.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<div class="small-12 column pinched">\n  <h3 class="dispatch-success">Successfully Swooned.</h3>\n  <p>' +
+__p += '<div class="small-12 column pinched bounceInDown">\n  <h3 class="dispatch-success">Successfully Swooned.</h3>\n  <p>' +
 ((__t = ( message )) == null ? '' : __t) +
 '</p>\n</div>\n';
 
