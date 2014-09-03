d43c916e0557b4776bfbf356f5611879dd2759e3
Dane O'Connor <dane.oconnor@gmail.com>
2014-09-02 23:06:34 -0400

    Add assets to repository so deploying to heroku is easier

---
+++ app/frontend/dist/js/application.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('form input[name="subreddit"]').focus();
+});
+
+var CharmView = Backbone.View.extend({
+
+  el: $("body"),
+
+  events: {
+    "submit form": "submit"
+  },
+
+  initialize: function() {
+    this.$form = $("form");
+  },
+
+  submit: function(e) {
+    $.ajax({
+      url: "/charm",
+      type: "POST",
+      accepts: "application/json",
+      data: this.$form.serialize(),
+      success: function(data) {
+        $(".last-charm").html(JST["assets/templates/last_charm.html"](data));
+      }
+    });
+
+    return false; // don't actually submit of the form.
+  },
+});
+
+view = new CharmView();
---
+++ app/frontend/dist/js/templates.js
@@ -0,0 +1,13 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["assets/templates/last_charm.html"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="small-12 column pinched">\n  <h3 class="dispatch-success">Successfully Swooned.</h3>\n  <p>' +
+((__t = ( message )) == null ? '' : __t) +
+'</p>\n</div>\n';
+
+}
+return __p
+};
