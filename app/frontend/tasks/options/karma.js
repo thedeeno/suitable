99896051637aea2833317992e2448fe55a0133c8
Dane O'Connor <dane.oconnor@gmail.com>
2014-09-03 13:00:10 -0400

    Configure karma:unit task to run once and have less verbose output

--- app/frontend/tasks/options/karma.js
+++ app/frontend/tasks/options/karma.js
@@ -15,12 +15,9 @@ module.exports = function(config) {
 
         port: 9876,
         colors: true,
-        autoWatch: true,
-        singleRun: false,
+        singleRun: true,
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: 'ERROR',
 
         browsers: ['Firefox']
       }
